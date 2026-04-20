86dc5f83d458ad66cd5f9cdbf4b19a4e6caf5e96
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-04-20 21:32:43 +0200

    Add gallery and form functionality with SimpleLightbox integration

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,5 @@
-import { onEmailInput, onLoadData, onSubmitForm } from './js/handler';
-import { refs } from './js/refs';
+import { onEmailInput, onLoadData, onSubmitForm } from './handler';
+import { refs } from './refs';
 document.addEventListener('DOMContentLoaded', onLoadData);
 refs.user_form.addEventListener('input', onEmailInput);
 refs.user_form.addEventListener('submit', onSubmitForm);
