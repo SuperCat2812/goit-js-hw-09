3c56ef891e1af8617d0ca57f3f2db111c8a76655
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-04-21 19:23:32 +0200

    Fix typo in button selector and reset formData on submit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,7 +2,7 @@ const refs = {
   user_email: document.querySelector('input[type="email"]'),
   user_message: document.querySelector('textarea'),
   user_form: document.querySelector('.feedback-form'),
-  user_btn: document.querySelector('button[type="submit]'),
+  user_btn: document.querySelector('button[type="submit"]'),
 };
 
 const STORAGE_KEYS = {
@@ -47,8 +47,8 @@ function onSubmitForm(event) {
 
     localStorage.removeItem(STORAGE_KEYS.USER_FORM);
     refs.user_form.reset();
-    formData.email = email;
-    formData.message = message;
+    formData.email = '';
+    formData.message = '';
   }
 }
 
